8029b82b06254f59c78130651f740a9462513e91
PauChoqueneira <pchoqueneira@unsa.edu.pe>
2020-07-31 15:48:05 -0500

    se creo la app usuario para manejar la creacion de usuarios con acceso diferente al admin de django por default

--- restaurante/urls.py
+++ restaurante/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('persona/', include('persona.urls')),
     path('accounts/', include('accounts.urls')),
     path('platos/', include('platos.urls')),
+    path('usuario/', include('usuario.urls')),
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
