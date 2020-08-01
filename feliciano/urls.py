07017de0a4ad0678d5f5af04655220a8f71c86b4
Jose rodriguez Mercado <jrodriguezme@unsa.edu.pe>
2020-08-01 05:22:54 -0500

    se implemento paginas y se configuro vista en la app feliciano y arreglo error de la anterior compilacion

--- feliciano/urls.py
+++ feliciano/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-	path('',views.index, name="index")
+	path('',views.index, name="index"),
+	path('nosotros',views.nosotros, name="nosotros"),
+	path('blog',views.blog, name="blog"),
+	path('contactanos',views.contactanos, name="contactanos"),
+	path('menu',views.menu, name="menu"),
 ]
--- feliciano/views.py
+++ feliciano/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from platos.models import Platos
+from persona.models import Personal
 # Create your views here.
 
 def index(request):
 	platos=Platos.objects.all()
-	return render(request,'index.html',{'platos':platos})
+	personal=Personal.objects.all()
+	return render(request,'index.html',{'platos':platos,'personal':personal})
+def nosotros(request):
+	return render(request,'about.html')
+
+def blog(request):
+	return render(request,'blog.html')
+
+def contactanos(request):
+	return render(request,'contact.html')
+
+def menu(request):
+	return render(request,'menu.html')
