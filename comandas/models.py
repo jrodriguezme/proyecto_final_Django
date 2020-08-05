c3fd9619f20f7508050fc1d9d5e8c406bebeba88
Jose rodriguez Mercado <jrodriguezme@unsa.edu.pe>
2020-08-04 22:26:07 -0500

    se creo modelos para comandas

--- comandas/models.py
+++ comandas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+
+class Formato(models.Model):
+	numero_comanda		= Formato.objects.count()+1
+	fecha_hoy			= models.DateField(default = date.today)
+	numero_mesa			= models.IntegerField()
+	numero_comensales	= models.IntegerField()
+	id_camarero			= models.CharField(max_length=100)
+	id_platos			= ArrayField(ArrayField(models.IntegerField(),default=[]))
