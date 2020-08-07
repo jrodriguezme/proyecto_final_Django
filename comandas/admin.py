ab03da8b4f0bb7af63a8fdf86de912f7b86a4654
Jose rodriguez Mercado <jrodriguezme@unsa.edu.pe>
2020-08-07 06:50:05 -0500

    se completo el crud de comandas eliminar listar y modificar

--- comandas/admin.py
+++ comandas/admin.py
@@ -2,9 +2,5 @@ from django.contrib import admin
 from .models import Formato
 
 # Register your models here.
-<<<<<<< HEAD
-=======
-from .models import Formato
 
->>>>>>> 78e6dc0da1fb326390a96f33843606dc5227bf27
 admin.site.register(Formato)
--- comandas/models.py
+++ comandas/models.py
@@ -1,34 +1,26 @@
 from django.db import models
 import datetime
 from django.utils import timezone
-from django.apps import apps
+from django.urls import reverse
 # Create your models here.
-# genero = (
-#        ('femenino','FEMENINO'),
-#        ('masculino', 'MASCULINO'),
-#        ('otro','Prefiero no decirlo'),
-# )
-<<<<<<< HEAD
+
+
 def hora():
-      hour = timezone.now()
-      formatedHour = hour.strftime("%H:%M:%S")
-      return formatedHour
+	hour = timezone.now()
+	formatedHour = hour.strftime("%H:%M:%S")
+	return formatedHour
+
 def numero():
-	numero=Formato.objects.count
-=======
+	numero=Formato.objects.count()
+	return numero+1
 
-
-def contador():
-	Formato=apps.get_model(app_label='comandas',model_name='Formato')
-	return Formato.objects.count()
-	
->>>>>>> 78e6dc0da1fb326390a96f33843606dc5227bf27
 class Formato(models.Model):
-	numero_comanda		= numero
+	numero_comanda		= models.IntegerField(default=numero)
 	hora_marca 			= models.CharField(max_length=50, default=hora)
-	fecha_hoy			= datetime.date.today()
-	numero_mesa			= models.IntegerField()
-	numero_comensales	= models.IntegerField()
+	fecha_hoy			= models.CharField(max_length=50,default=datetime.date.today())
+	numero_mesa			= models.IntegerField(default=0)
+	numero_comensales	= models.IntegerField(default=0)
 	id_camarero			= models.CharField(max_length=100)
 	id_platos			= models.CharField(max_length=100)
-
+	def get_absolute_url(self):
+		return reverse('comandas:detalles_comandas', kwargs = {'pk': self.id})
